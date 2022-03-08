c00ae9429c4eb480ec4d37b4adda5a39556cf889
ZackOBrien <obrienzj19@gcc.edu>
2022-03-07 21:35:26 -0500

    addClass method for the CourseList class. Will need helper methods in the future.

--- theProject/src/CourseList.java
+++ theProject/src/CourseList.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class CourseList {
+    public class course{
+        //place holder
+    }
+    public static void addClass(course course, ArrayList<course> Schedule){
+        //method needed here for to check for conflicition in schedule
+        Schedule.add(course);
+        //possibly a remove from grand course list so you can add double of a course
+    }
 }
