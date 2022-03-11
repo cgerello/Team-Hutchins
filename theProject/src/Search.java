31e0212ea195d71f6d7eabeb142e129e1487064a
MATTHEWSSM19 <smatthews4060@outlook.com>
2022-03-10 19:44:21 -0500

    Worked on Search Class

--- theProject/src/Search.java
+++ theProject/src/Search.java
@@ -2,26 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Search {
-    String searchInput;
+    static String searchInput; //Change from static later
 
     public Search(String searchInput){
         this.searchInput = searchInput;
+        //getResults();
     }
 
-
-
-    public static void main(String[] args){ //Temporary main for testing
+    public static void getResults(String searchInput){ //Get rid of static afterwards and return string, get rid of parameter
         try {
-            File classFile = new File("classFile.txt");
+            File classFile = new File("theProject/src/classFile.txt");
             Scanner classScan = new Scanner(classFile);
+            String data = "";
             while (classScan.hasNextLine()) {
-                String data = classScan.nextLine();
-                System.out.println(data);
+                data = classScan.nextLine();
+                Scanner dataScan = new Scanner(data);
+                dataScan.useDelimiter(",");
+                while (dataScan.hasNext()){
+                    String theString = dataScan.next();
+                    System.out.println(dataScan.next());
+                    if (theString.equalsIgnoreCase(searchInput)){
+                        System.out.println("Match found for: " + searchInput);
+                    }
+                }
+                dataScan.close();
             }
             classScan.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public void addFilter(){ //Work on this
+
+    }
+
+    public static void main(String[] args){ //Temporary main for testing
+        getResults("HAL");
     }
 }
