034d4429ec58d52d1706e751d521df2b6f1c63c3
MATTHEWSSM19 <smatthews4060@outlook.com>
2022-03-12 19:01:44 -0500

    Finished Search Class, Waiting for "Course" class to be made so that it can return an ArrayList of results

--- theProject/src/Search.java
+++ theProject/src/Search.java
@@ -9,22 +9,37 @@ public class Search {
         //getResults();
     }
 
-    public static void getResults(String searchInput){ //Get rid of static afterwards and return string, get rid of parameter
+    public static void getResults(String searchInputWithSpace){ //Get rid of static afterwards and return string, get rid of parameter
         try {
             File classFile = new File("theProject/src/classFile.txt");
             Scanner classScan = new Scanner(classFile);
-            String data = "";
+            String course;
+            int index = 0;
+            String searchInput = searchInputWithSpace.replace(" ", "");
             while (classScan.hasNextLine()) {
-                data = classScan.nextLine(); //grabs the line of code (the course info)
-                Scanner dataScan = new Scanner(data); //Creates a new scanner to read the line
-                dataScan.useDelimiter(",");
-                while (dataScan.hasNext()){
-                    System.out.println(dataScan.next());
-                    if (dataScan.next().equalsIgnoreCase(searchInput)){
-                        System.out.println("Match found for: " + searchInput);
+                course = classScan.nextLine(); //grabs the line of code (the course info)
+                Scanner courseScan = new Scanner(course); //Creates a new scanner to read the line
+                courseScan.useDelimiter(",");
+                while (courseScan.hasNext()){ //Only search by course code and full course name
+                    String data = courseScan.next().replace(" ", "");
+                    if(data.equalsIgnoreCase("CourseCode")){
+                        break;
                     }
+                    if (data.equalsIgnoreCase(searchInput) && index == 0 ||
+                            data.toLowerCase().contains(searchInput.toLowerCase()) && index == 0){ //User is searching by course code
+                        System.out.println("Match found for: " + searchInput);
+                        System.out.println("Here is the course: " + course + "\n");
+                        break;
+                    }
+                    else if (data.equalsIgnoreCase(searchInput) && index == 2){ //User is searching by course name
+                        System.out.println("Match found for: " + searchInput);
+                        System.out.println("Here is the course: " + course + "\n");
+                        break;
+                    }
+                    index++;
                 }
-                dataScan.close();
+                index = 0;
+                courseScan.close();
             }
             classScan.close();
         } catch (FileNotFoundException e) {
@@ -38,6 +53,6 @@ public class Search {
     }
 
     public static void main(String[] args){ //Temporary main for testing
-        getResults("HAL");
+        getResults("abrd");
     }
 }
