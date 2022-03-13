bac9a3491e6a60286ee705bf566185c2fb760cbd
Grace Erk <graceerk21@gmail.com>
2022-03-13 15:34:38 -0400

    Made Main output formatting less visually confusing

--- theProject/src/Main.java
+++ theProject/src/Main.java
@@ -12,10 +12,17 @@ public class Main {
 
 
     static void printScreen(){
-        System.out.println("___________________________________________________");
-        System.out.println("Team Hutchins\t\t\tCLASS SCHEDULING ASSISTANT");
-        System.out.println("___________________________________________________\n");
+        System.out.println("\n___________________________________________________");
+        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        System.out.println(": Team Hutchins\t\t\tCLASS SCHEDULING ASSISTANT :");
+        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
     }
+
+    static void printEndScreen(){
+        System.out.println("___________________________________________________\n");
+
+    }
+
 
     public static void main (String[] args) {
         //CourseList cl = new CourseList();
@@ -26,46 +33,54 @@ public class Main {
         System.out.println("- Welcome to the Class Scheduling Assistant!");
         System.out.println("- Type what you'd like to do:\n");
         System.out.println("login\t\tsign up\t\tquit\n");
-        System.out.println("___________________________________________________\n");
+        System.out.print(">");
         String command = mainScn.nextLine();
 
         while (!command.equals("quit")){
             if(command.equals("login")){
                 //TODO
                 System.out.println("- TODO: login");
+                printEndScreen();
             }
            else if(command.equals("sign up")){
                 //TODO
                 System.out.println("- TODO: sign up");
+                printEndScreen();
             }
 
            else if(command.equals("add")){
                 //TODO
                 System.out.println("- TODO: add");
+                printEndScreen();
             }
 
             else if(command.equals("remove")){
                 //TODO
                 System.out.println("- TODO: remove");
+                printEndScreen();
             }
 
             else if(command.equals("undo")){
                 //TODO
                 System.out.println("- TODO: undo");
+                printEndScreen();
             }
 
             else if(command.equals("redo")){
                 //TODO
                 System.out.println("- TODO: redo");
+                printEndScreen();
             }
 
             else if(command.equals("list")){
                 printCommands();
+                printEndScreen();
             }
             //etc
 
            else {
                 System.out.println("- Command not recognized.");
+                printEndScreen();
             }
 
 
@@ -73,9 +88,9 @@ public class Main {
 
            //if user is signed in...
             System.out.println("- Type what you'd like to do:");
-            System.out.println("  (or 'list' for valid commands)\n");
+            System.out.println("  (or type 'list' to see valid commands)\n");
+            System.out.print(">");
             command = mainScn.nextLine();
-            System.out.println("___________________________________________________\n");
         };
 
         System.out.println("- Goodbye.\n");
